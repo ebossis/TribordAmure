1d39b9398f4f5722aa5defb8e4b78acb2eed2680
Cyberange98 <sandra.bossis@gmail.com>
2026-04-06 23:16:33 +1100

    Update login.js

--- login.js
+++ login.js
@@ -1,12 +1,33 @@
+// Au chargement → remplir si mémorisé
+window.addEventListener('DOMContentLoaded', () => {
+    const savedEmail = localStorage.getItem('remember_email');
+    const remember = localStorage.getItem('remember_me') === 'true';
+
+    if (remember && savedEmail) {
+        document.getElementById('email').value = savedEmail;
+        document.querySelector('.remember-me input').checked = true;
+    }
+});
+
 document.getElementById('loginForm').addEventListener('submit', function(e) {
     e.preventDefault();
     
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
+    const remember = document.querySelector('.remember-me input').checked;
 
-    // Simulation pour tes tests (tu donneras ça à Erwan plus tard)
     if (email === "admin@tribord.nc" && password === "lagon2026") {
+
+        if (remember) {
+            localStorage.setItem('remember_email', email);
+            localStorage.setItem('remember_me', true);
+        } else {
+            localStorage.removeItem('remember_email');
+            localStorage.removeItem('remember_me');
+        }
+
         window.location.href = 'admin.html';
+
     } else {
         alert("Oups ! Identifiants incorrects. Demandez à Erwan ou réessayez !");
     }
